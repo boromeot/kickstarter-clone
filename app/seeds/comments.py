4c3727a8552207c7af7b9eca365ee1b25b1c6b3f
Tyler Boromeo <boromeotyler@gmail.com>
2021-10-08 16:08:31 -0700

    updated comments / project / users relationships

--- app/seeds/comments.py
+++ app/seeds/comments.py
@@ -2,15 +2,15 @@ from app.models import db, Comment
 
 def seed_comments():
   comments = []
-  comments[0] = Comment(description="Simply amazing", project_id=1, user_id=1)
-  comments[1] = Comment(description="What a wonderful idea", project_id=2, user_id=2)
-  comments[2] = Comment(description="wack", project_id=3, user_id=3)
+  comments.append(Comment(description="Simply amazing", project_id=1, user_id=1))
+  comments.append(Comment(description="What a wonderful idea", project_id=2, user_id=2))
+  comments.append(Comment(description="wack", project_id=3, user_id=3))
 
 
   for comment in comments:
     db.session.add(comment)
 
-  db.session.commit
+  db.session.commit()
 
 
 def undo_comments():
