5f6d2853c844300091de3cd8aae3f1945b7a9045
Tyler Boromeo <boromeotyler@gmail.com>
2021-10-08 20:18:28 -0700

    added Updates seeder file and data

--- app/seeds/__init__.py
+++ app/seeds/__init__.py
@@ -2,6 +2,7 @@ from flask.cli import AppGroup
 from .users import seed_users, undo_users
 from .projects import seed_projects, undo_projects
 from .comments import seed_comments, undo_comments
+from .updates import seed_updates, undo_updates
 
 # Creates a seed group to hold our commands
 # So we can type `flask seed --help`
@@ -14,6 +15,7 @@ def seed():
     seed_users()
     seed_projects()
     seed_comments()
+    seed_updates()
     # Add other seed functions here
 
 
@@ -23,4 +25,5 @@ def undo():
     undo_users()
     undo_projects()
     undo_comments()
+    undo_updates()
     # Add other undo functions here
