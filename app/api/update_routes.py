ea659e525817974b6e48b8d2d5d60cfc547e838d
xanadel <trevinwoods41@gmail.com>
2021-10-11 18:30:17 -0500

    added updates component to project page. Also added functionality to create an update

--- app/api/update_routes.py
+++ app/api/update_routes.py
@@ -10,23 +10,10 @@ update_routes = Blueprint('updates', __name__)
 @update_routes.route('/', methods=['POST'])
 def create_update():
     print("Test------------------------")
-    # interesting ways to grab data when not using action or method in form submission on frontend components
-    # body = request.get_json()
-   
+    # print(request.json['title'])
     
-    print(request.json['title'])
-    # print(body['title'])
-    # print(body['description'])
-    # print(body['user_id'])
-    # print(body['project_id'])
-    # print(Update(
-    #         title=body['title'],
-    #         description=body['description'], 
-    #         project_id=body['project_id'],
-    #         user_id=body['user_id'],
-    #     ))
-
     if request.method == 'POST':
+        
         new_update = Update(
             title=request.json['title'],
             description=request.json['description'], 
@@ -39,23 +26,3 @@ def create_update():
         return "Success"
     else:
         return 'Bad Data'
-
-# description = str(body['description'])
-    # title = str(body['title'])
-    # user_id = body['user_id']
-    # project_id = body['project_id']
-    # db.session.add(Update(
-    #     title=title,
-    #     description=description, 
-    #     project_id=project_id,
-    #     user_id=user_id
-    # ))
-
-    # title = request.form.get('title')
-    # print(request.form.get('title'))
-    # description = request.form.get('description')
-    # print(request.form.get('description'))
-
-
-    # print(title, description, body['user_id'], body['project_id'], "DATAAAAAAAAAAAAAAAAAAAAAAaa")
-    # print(title, description, "DATAAAAAAAAAAAAAAAAAAAAAA Twooooooooooooooo")
