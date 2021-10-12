26e36aebc151f23636fee24e5e6a0358dfed2f85
Tyler Boromeo <boromeotyler@gmail.com>
2021-10-12 16:17:30 -0700

    user must now be logged in to see createProjectPage

--- react-app/src/components/CreateProjectPage/index.js
+++ react-app/src/components/CreateProjectPage/index.js
@@ -1,6 +1,14 @@
 import React from 'react';
+import { useSelector } from 'react-redux';
+import { Redirect } from 'react-router-dom';
 
 const CreateProjectPage = () => {
+  const user = useSelector(state => state.session.user);
+
+  if (!user) {
+    return <Redirect to='/login' />
+  }
+
   return 'Hello from create project';
 }
 
