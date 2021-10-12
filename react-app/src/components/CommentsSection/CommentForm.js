8a7e604806ad283cb520744c29fe400cdd35c81d
Tyler Boromeo <boromeotyler@gmail.com>
2021-10-11 19:58:54 -0700

    added onSubmit function and dispatch. Now need to write dispatcher and post comment route

--- react-app/src/components/CommentsSection/CommentForm.js
+++ react-app/src/components/CommentsSection/CommentForm.js
@@ -1,15 +1,22 @@
 import React, { useState } from "react";
+import { useDispatch } from "react-redux";
 import './CommentForm.css';
 
 const CommentForm = () => {
   const [description, setDescription] = useState('');
+  const dispatch = useDispatch();
 
   const updateDescription = (e) => {
     setDescription(e.target.value);
   };
 
+  const onSubmit = async e => {
+    e.preventDefault();
+
+  }
+
   return (
-    <form className='form-container'>
+    <form onSubmit={onSubmit} className='form-container'>
       <div>
           <textarea
             name='description'
