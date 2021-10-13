ad79efccd178d46a4dd34004ad5e2ee3329a49da
Tyler Boromeo <boromeotyler@gmail.com>
2021-10-13 13:13:10 -0700

    prev button style

--- react-app/src/components/CreateProjectPage/index.js
+++ react-app/src/components/CreateProjectPage/index.js
@@ -53,8 +53,17 @@ const CreateProjectPage = () => {
       <div>
         <div className='start-form-spacer'>
           <div className='start-form-button-container'>
-            <button onClick={prevStep}>Prev</button>
-            <button className='start-form-next-btn' onClick={nextStep}>Next</button>
+            {currentStep < totalSteps &&
+              <button className='start-form-next-btn' onClick={nextStep}>Next</button>
+            }
+            {currentStep > 1 &&
+              <span className='start-form-back-btn' onClick={prevStep}>
+                <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-arrow-left" viewBox="0 0 16 16">
+                  <path fill-rule="evenodd" d="M15 8a.5.5 0 0 0-.5-.5H2.707l3.147-3.146a.5.5 0 1 0-.708-.708l-4 4a.5.5 0 0 0 0 .708l4 4a.5.5 0 0 0 .708-.708L2.707 8.5H14.5A.5.5 0 0 0 15 8z"/>
+                </svg>
+                <span className='start-form-back-btn-text'>Prev</span>
+              </span>
+            }
           </div>
         </div>
       </div>
