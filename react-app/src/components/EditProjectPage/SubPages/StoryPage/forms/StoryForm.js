3eb597a5a4e262bd2b0d8f9166b3cca5d5134635
Tyler Boromeo <boromeotyler@gmail.com>
2021-10-15 12:44:54 -0700

    restructed files

--- react-app/src/components/EditProjectPage/SubPages/StoryPage/forms/StoryForm.js
+++ react-app/src/components/EditProjectPage/SubPages/StoryPage/forms/StoryForm.js
@@ -1,21 +1,16 @@
 import React from "react";
 import '../../FormWrapper.css';
-import '../StoryForm.css';
+import '../../RichTextEditor.css';
 
 const StoryForm = () => {
   return (
-    // <div className='edit-page-spacer'>
-    //   <div className='form-info-container'>
-    //     ggg
-    //   </div>
-    // </div>
     <div className='form-wrapper-border'>
       <div className='edit-page-spacer'>
         <div className='form-wrapper-spacer'>
-          <div className='form-info-container' id='story-form-info-container'>
+          <div className='form-info-container story-form-info-container'>
             <h2 className='form-info-header'>Project description</h2>
-            <p className='form-info' id='story-form-info'>Describe what you're raising funds to do, why you care about it, how you plan to make it happen, and who you are. Your description should tell backers everything they need to know. If possible, include images to show them what your project is all about and what rewards look like.</p>
-            <div className='story-form'>
+            <p className='form-info story-form-info'>Describe what you're raising funds to do, why you care about it, how you plan to make it happen, and who you are. Your description should tell backers everything they need to know. If possible, include images to show them what your project is all about and what rewards look like.</p>
+            <div className='rich-text-editor'>
               Form goes here
             </div>
           </div>
