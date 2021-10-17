1fd0b5b417be2c1e30d5dc2eb2bfed97f183e74c
xanadel <trevinwoods41@gmail.com>
2021-10-17 10:28:33 -0500

    added second FAQ Component to be rendered

---
+++ react-app/src/components/ProjectPage/FAQListComponent/index.js
@@ -0,0 +1,12 @@
+
+
+import './FAQListComponent.css'
+
+
+export default function FAQListComponent() {
+    return (
+        <div>
+            <h1> FAQListComponent </h1>
+        </div>
+    )
+}
