1312fc8595e09c14a88992c6661e9285ace6c58e
Tyler Boromeo <boromeotyler@gmail.com>
2021-11-22 17:26:04 -0800

    updated category nav bar on splash page

--- react-app/src/components/SplashPageComponent/SplashNav.js
+++ react-app/src/components/SplashPageComponent/SplashNav.js
@@ -7,12 +7,14 @@ import './SplashPageComponent.css'
 export default function SplashNav() {
     const tags = useSelector(state => state.tags);
     return (
-        <div className='grid-container'>
-            <nav className='category-nav'>
-                {tags?.map(tag => {
-                    return <NavLink className='' to='#' key={tag.id}>{tag.title}</NavLink>
-                })}
-            </nav>
+        <div className='border-bottom-gray'>
+            <div className='grid-container'>
+                <nav className='category-nav'>
+                    {tags?.map(tag => {
+                        return <NavLink className='category-link' to='#' key={tag.id}>{tag.title}</NavLink>
+                    })}
+                </nav>
+            </div>
         </div>
     )
 }
