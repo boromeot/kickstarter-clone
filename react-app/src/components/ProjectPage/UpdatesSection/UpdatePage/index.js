88f980234804bd480d277d4080fd9f6a812b9d17
Tyler Boromeo <boromeotyler@gmail.com>
2021-11-27 17:17:45 -0800

    all updates button

--- react-app/src/components/ProjectPage/UpdatesSection/UpdatePage/index.js
+++ react-app/src/components/ProjectPage/UpdatesSection/UpdatePage/index.js
@@ -3,16 +3,22 @@ import { NavLink } from 'react-router-dom';
 import './UpdatePage.css';
 
 const UpdatePage = () => {
+
   return (
     <div className='grid-container py6'>
       <div className='update-page'>
         <div className='pb8'>
-          <NavLink className='update-page-all-updates' to='#'>
-            <div>
-              <span>All updates</span>
+          <NavLink className='update-page-all-updates btn-secondary' to='#'>
+            <div className='flex-center'>
+              <svg className='icon-small ' viewBox='0 0 60 60'>
+                <path fill-rule="nonzero" d="M14 30l26 26 5-5-21-21L45 9l-5-5"></path>
+              </svg>
+              <span className='ml2'>All updates</span>
             </div>
           </NavLink>
         </div>
+        <article>
+        </article>
       </div>
     </div>
   )
