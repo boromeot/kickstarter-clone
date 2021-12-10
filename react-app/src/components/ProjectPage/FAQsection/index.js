9bced7d8d9053162d827bbb0831aca154bca3ce8
Tyler Boromeo <boromeotyler@gmail.com>
2021-12-09 23:38:16 -0800

    fixed style when there are no faqs

--- react-app/src/components/ProjectPage/FAQsection/index.js
+++ react-app/src/components/ProjectPage/FAQsection/index.js
@@ -12,25 +12,32 @@ const FAQsection = ({ faqs }) => {
             <h3 className='font-size--24 font-weight--400'>Frequently Asked Questions</h3>
           </div>
         </div>
-        <div className='grid-row flex'>
           {faqs?.length > 0
-            ? <div className='pz3 width-8-12'>
-                {faqs.map(faq => {
-                  return <FAQ faq={faq} />
-                })}
+            ? <div className='grid-row flex'>
+                <div className='pz3 width-8-12'>
+                    {faqs.map(faq => {
+                      return <FAQ faq={faq} />
+                    })}
+                  </div>
+                  <div className='pz3 h20p ml7 border-left border3px mb5 width-4-12'>
+                    <p className="mb3 font-size--14">Don't see the answer to your question? Ask the project creator directly.</p>
+                    <span>
+                      <NavLink to='#' className='btn btn-medium btn-tertiary'>
+                        Ask a question
+                      </NavLink>
+                    </span>
+                  </div>
               </div>
             :
-              <p className='mb3 font-size--16'>Looks like there aren't any frequently asked questions yet.</p>
+              <div className="grid-row">
+                <p className='mb3 font-size--16'>Looks like there aren't any frequently asked questions yet.</p>
+                <span>
+                  <NavLink to='#' className='btn btn-medium btn-tertiary'>
+                    Ask a question
+                  </NavLink>
+                </span>
+              </div>
           }
-          <div className='pz3 h20p ml7 border-left border3px mb5 width-4-12'>
-            <p className="mb3 font-size--14">Don't see the answer to your question? Ask the project creator directly.</p>
-            <span>
-              <NavLink to='#' className='btn btn-medium btn-tertiary'>
-                Ask a question
-              </NavLink>
-            </span>
-          </div>
-        </div>
       </div>
     </div>
   )
