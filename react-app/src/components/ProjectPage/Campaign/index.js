de8a91ac8b3f36e16607f748e23b0504bcdadfae
Tyler Boromeo <boromeotyler@gmail.com>
2021-12-10 19:19:36 -0800

    more compaign positioning

--- react-app/src/components/ProjectPage/Campaign/index.js
+++ react-app/src/components/ProjectPage/Campaign/index.js
@@ -1,4 +1,5 @@
 import React, { useEffect } from "react";
+import { NavLink } from "react-router-dom";
 import "./Campaign.css";
 
 function Campaign({ campaign }){
@@ -11,13 +12,22 @@ function Campaign({ campaign }){
   return(
     <div className="grid-container pr3 py8">
       <div className="grid-row">
-        <div className="float-left width-8-12">
+        <div className="float-left width-8-12 pz3">
           <div className="pz3">
             <div id="campaign">
               <div className="grid-row">
-                <nav className="sticky ">
-
+                <nav className="sticky pb4 pl0 pr3 float-left width-2-12">
+                  <div>
+                    <NavLink to="#" className="font-size--13 bold lsp-2 pb3 uppercase">Story</NavLink>
+                  </div>
                 </nav>
+                <div className="pz3 float-left width-10-12">
+                  <div className="m-auto " style={{maxWidth: '680px'}}>
+                    <div id="story" className="mb3">
+                      <h3 className="mb3 font-weight--400 font-size--21">Story</h3>
+                    </div>
+                  </div>
+                </div>
               </div>
             </div>
           </div>
