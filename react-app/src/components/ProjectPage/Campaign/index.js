725fde9300819d1d57ec7a1e71e2921785e9dcce
Tyler Boromeo <boromeotyler@gmail.com>
2021-12-10 19:47:11 -0800

    formatted rte content

--- react-app/src/components/ProjectPage/Campaign/index.js
+++ react-app/src/components/ProjectPage/Campaign/index.js
@@ -4,10 +4,10 @@ import "./Campaign.css";
 
 function Campaign({ campaign }){
 
-  // useEffect(() => {
-  //   let campaignText = document.getElementsByClassName("campaign-description");
-  //   campaignText[0].innerHTML = campaign;
-  // })
+  useEffect(() => {
+    let campaignText = document.getElementById("rte-content");
+    campaignText.innerHTML = campaign;
+  })
 
   return(
     <div className="grid-container pr3 py8">
@@ -18,13 +18,16 @@ function Campaign({ campaign }){
               <div className="grid-row">
                 <nav className="sticky pb4 pl0 pr3 float-left width-2-12">
                   <div>
-                    <NavLink to="#" className="font-size--13 bold lsp-2 pb3 uppercase">Story</NavLink>
+                    <NavLink to="#" className="block font-size--13 bold lsp-2 pb3 green border-bottom">Story</NavLink>
                   </div>
                 </nav>
                 <div className="pz3 float-left width-10-12">
                   <div className="m-auto " style={{maxWidth: '680px'}}>
                     <div id="story" className="mb3">
                       <h3 className="mb3 font-weight--400 font-size--21">Story</h3>
+                    </div>
+                    <div id="rte-content">
+
                     </div>
                   </div>
                 </div>
