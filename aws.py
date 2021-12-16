c7a2fe80a24927a707406bed71de09d84de9fa38
Tyler Boromeo <boromeotyler@gmail.com>
2021-12-15 20:33:02 -0800

    added function to create unique file names for bucket

--- aws.py
+++ aws.py
@@ -7,3 +7,16 @@ s3 = boto3.client(
    aws_access_key_id=os.environ.get("S3_KEY"),
    aws_secret_access_key=os.environ.get("S3_SECRET")
 )
+
+import uuid
+
+ALLOWED_EXTENSIONS = {"pdf", "png", "jpg", "jpeg", "gif"}
+
+def allowed_file(filename):
+    return "." in filename and \
+           filename.rsplit(".", 1)[1].lower() in ALLOWED_EXTENSIONS
+
+def get_unique_filename(filename):
+    ext = filename.rsplit(".", 1)[1].lower()
+    unique_filename = uuid.uuid4().hex
+    return f"{unique_filename}.{ext}"
