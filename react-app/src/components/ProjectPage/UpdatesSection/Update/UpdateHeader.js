167439fd79995d49b86bc45ede4483a045a15a13
Tyler Boromeo <boromeotyler@gmail.com>
2021-12-26 05:48:35 -0800

    added default profile pic, removed about links

--- react-app/src/components/ProjectPage/UpdatesSection/Update/UpdateHeader.js
+++ react-app/src/components/ProjectPage/UpdatesSection/Update/UpdateHeader.js
@@ -3,13 +3,12 @@ import React from "react";
 const UpdateHeader = ({ update, index}) => {
   return (
     <header>
-      <div onClick={() => console.log(typeof index)}>log</div>
       <div className='update-counter-container'>
         <span className='update-counter'>{`Update #${+index + 1}`}</span>
       </div>
       <h2 className='update-header'>{update?.title}</h2>
       <div className='update-author-container '>
-        <img className='update-author-icon' alt=""/>
+        <img src='https://www.kindpng.com/picc/m/451-4517876_default-profile-hd-png-download.png' className='update-author-icon' alt=""/>
         <div className='pl2'>
           <div>
             {update?.username}
